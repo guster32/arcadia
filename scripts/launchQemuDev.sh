ecf218877860c24a5486e742eeb33fb8ef204f3f
Gustavo Branco <guster32@gmail.com>
2024-03-09 09:58:41 -0800

    Gets mamedev to build

--- scripts/launchQemuDev.sh
+++ scripts/launchQemuDev.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+#-device AC97 
 qemu-system-x86_64 --enable-kvm \
         -cpu IvyBridge \
         -machine type=q35,accel=kvm \
         -smp 4 \
         -kernel $HOME/qemux86-64/bzImage-qemux86-64.bin --append "root=/dev/sda console=ttys0 oprofile.timer=1 tsc=reliable no_timer_check rcupdate.rcu_expedited=1" \
         -drive file=$HOME/qemux86-64/core-image-arcadia-dev-qemux86-64.rootfs.ext4,index=0,media=disk,format=raw \
-        -device AC97 \
+        -device intel-hda \
         -m 8G \
 	-vga virtio \
         -usb \
